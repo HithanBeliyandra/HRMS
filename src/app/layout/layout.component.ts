80c8ccf50f8c1e546227fdf0be16b266999b109c
Hithan Beliyandra <hithan.beliyandra@quadgen.com>
2022-10-13 18:42:04 +0530

    Initial commit

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MediaObserver, MediaChange } from '@angular/flex-layout';
 import { Subscription } from 'rxjs';
 
+
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
+
 export class LayoutComponent implements OnInit, OnDestroy {
   sideNavOpened = true;
   sideNavMode: 'side' | 'over' = 'side';
